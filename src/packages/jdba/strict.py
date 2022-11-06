97f055fc4a293ec25e0de31c4f38522f87fb67a8
Henrique Moreira <h@serrasqueiro.com>
2022-11-06 13:37:30 +0000

    Schema validation, uniqueness

--- src/packages/jdba/strict.py
+++ src/packages/jdba/strict.py
@@ -41,6 +41,19 @@ class StrictSchema(jdba.jcommon.DataSchema):
             used[tname] = cases
         if not ndexing:
             return "Nothing to validate"
+        msg = self._validate_ordr(
+            (tname, cases, box, used),
+            ndexing, order,
+            debug
+        )
+        return msg
+
+    def _validate_ordr(self, tup:tuple, ndexing, order:list, debug):
+        assert isinstance(order, list)
+        tname, cases, box, used = tup
+        assert box, tname
+        if debug > 0:
+            print("::: validate_ordr():", overview(cases))
         for tname, cases, box in order:
             if debug > 0:
                 print(f"validate() tname={tname}, order is {overview(order)}")
@@ -86,7 +99,7 @@ def validate_unique_id(elems, infos) -> str:
         ids[an_id] = elem
     return ""
 
-def validate_uniqueness(elems, infos, depth, debug=1) -> str:
+def validate_uniqueness(elems, infos, depth, debug=DEBUG) -> str:
     if debug > 0:
         print(
             f"validate_uniqueness(), depth={depth}",
@@ -95,9 +108,13 @@ def validate_uniqueness(elems, infos, depth, debug=1) -> str:
     if depth > 10:
         # Avoid too much recursiveness
         return ""
-    _, _, _, method = infos
-    if method:
-        return ""	# TODO
+    boxname, acase, c_id, methods = infos
+    assert acase, boxname
+    assert c_id >= 1, boxname
+    #print(":::", boxname, "case id:", c_id, acase, "; method:", methods)
+    if methods:
+        msg = methods_validate(elems, infos, methods, depth, "uq")
+        return msg
     if isinstance(elems, dict):
         return validate_dict_keying(elems, infos)
     if not isinstance(elems, list):
@@ -108,11 +125,66 @@ def validate_uniqueness(elems, infos, depth, debug=1) -> str:
     if len(elems) == 1:
         return validate_uniqueness(elems[0], infos, depth+1)
     for idx, elem in enumerate(sorted(elems), 1):
-        print("idx:", idx, elem)
+        #print("idx:", idx, elem)
         if elem in ids:
             there = ids[elem]
             return f"Duplicate field idx={idx} {infos}: <<{there}>>"
         ids[elem] = idx
+    return ""
+
+def methods_validate(elems, infos, methods, depth, m_str) -> str:
+    """ Before calling micro_validate() """
+    assert m_str
+    assert depth <= 0
+    _, acase, c_id, _ = infos
+    assert c_id >= 1
+    for methd in methods:
+        an_id = methd["Id"]
+        if an_id < 1:
+            continue
+        uqs = methd.get("UniqueFields")
+        flds = uqs if uqs else []
+        #print("Debug:", acase, "; method:", methd, flds, ">>>", overview(elems), "<<<")
+        msg = validate_fields_uq(elems, infos, flds)
+        if msg:
+            return msg
+    return ""
+
+def validate_fields_uq(elems, quad, fields):
+    """ Validate field(s) uniqueness
+    """
+    _, acase, _, _ = quad
+    assert acase
+    for field in fields:
+        msg = micro_validate(elems, acase, field)
+        if msg:
+            return msg
+    return ""
+
+def micro_validate(elems, acase, field):
+    #print("VALIDATE:", acase, "; field:", field)
+    msgs = []
+    used = {}
+    for item in elems:
+        an_id = item["Id"]
+        if an_id < 1:
+            continue
+        hint = f" ({an_id})"
+        fld_val = item[field]
+        if fld_val in used:
+            #print("Already used:", item, "; by:", used[fld_val])
+            msg = f"Duplicate {field}='{fld_val}'{hint}; {used[fld_val]}"
+            msgs.append(msg)
+        else:
+            used[fld_val] = item
+    #for key in sorted(used):
+    #    print(f":::# {acase}.{field} key={key}, as: {used[key]}")
+    if len(msgs) > 1:
+        msg = f"{len(msgs)} dups with {acase}.{field}, first: {msgs[0]}"
+        return msg
+    if msgs:
+        msg = msgs[0]
+        return msg
     return ""
 
 def validate_dict_keying(elems:dict, infos):
